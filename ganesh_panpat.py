f0d4eb503369d7b3c6d8274457e7dc2c682f647b
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-11-28 01:11:07 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -1,5 +1,21 @@
 import streamlit as st
 st.set_page_config(page_title="Algo App",layout="wide",initial_sidebar_state="expanded",)
+st.markdown("""
+        <style>
+               .css-18e3th9 {
+                    padding-top: 0rem;
+                    padding-bottom: 10rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+               .css-1d391kg {
+                    padding-top: 3.5rem;
+                    padding-right: 1rem;
+                    padding-bottom: 3.5rem;
+                    padding-left: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
 if 'user_name' not in st.session_state:
    st.session_state['user_name']="Guest"
    st.session_state['user_name']="Guest"
