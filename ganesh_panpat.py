f1ddc6e81e67cf39c89af915835be2cf8d54135a
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-11-27 18:49:51 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -4,6 +4,11 @@ import time
 st.set_page_config(page_title="Algo App",layout="wide",initial_sidebar_state="expanded", )
 st.header("Welcome Ganesh Panpat")
 current_time=st.empty()
+col1,col2=st.columns([0.5,9.5],2)
+with col1:
+  st.button("HI")
+with col2:
+  st.button("Ganesh")
 for i in range(60):
   current_time.text(datetime.datetime.now().replace(microsecond=0))
   time.sleep(1)
