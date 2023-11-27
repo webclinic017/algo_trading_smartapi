f1db5e63308bf4cee75d96f57e92ab021e8119ea
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-11-27 21:13:19 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -4,7 +4,6 @@ import time
 import pandas as pd
 import numpy as np
 st.set_page_config(page_title="Algo App",layout="wide",initial_sidebar_state="expanded", )
-st.header("Welcome Ganesh Panpat!!!")
 current_time=st.empty()
 c1,c2=st.columns([2,8])
 with c1:
@@ -17,6 +16,45 @@ with c1:
       bnf_pe=st.button(label="BNF PE")
 with c2:
    st.table(pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20))))
-for i in range(60):
-  current_time.text(datetime.datetime.now().replace(microsecond=0))
-  time.sleep(1)
+if 'user_name' not in st.session_state:
+   st.session_state['user_name']="Guest"
+   st.session_state['user_name']="Guest
+from SmartApi import SmartConnect
+from SmartApi import SmartWebSocket
+import threading; import pandas as pd
+import pandas_ta as pdta
+import json
+import requests
+import datetime
+from dateutil.tz import gettz
+import time
+import math
+import numpy
+import warnings
+import yfinance as yf
+import sys
+def get_user_pwd(user):
+  if user=='Ganesh': username = 'G93179'; pwd = '4789'; apikey = 'CjOKjC5g'; token='U4EAZJ3L44CNJHNUZ56R22TPKI'
+  elif user=='Kalyani': username = 'K205244'; pwd = '4789'; apikey = 'lzC7yJmt'; token='YDV6CJI6BEU3GWON7GZTZNU3RM'
+  elif user=="Akshay": username='A325394'; pwd='1443'; apikey='OeSllszj'; token='G4OKBQKHXPS67EN2WMVP3TZ7X4'
+  return username,pwd,apikey,token,user
+username,pwd,apikey,token,user=get_user_pwd("Ganesh")
+obj=SmartConnect(api_key=apikey)
+st.cache_resource
+def angel_login():
+  try:
+    FEED_TOKEN = None;TOKEN_MAP = None;SMART_API_OBJ = None
+    global user_name
+    data = obj.generateSession(username,pwd,pyotp.TOTP(token).now())
+    refreshToken= data['data']['refreshToken']
+    feedToken=obj.getfeedToken()
+    userProfile= obj.getProfile(refreshToken)
+    aa= userProfile.get('data')
+     st.session_state['user_name']=aa.get('name').title()
+     st.session_state['login_time]=datetime.datetime.now()
+    print('Welcome',aa.get('name').title(),'...')
+    user=aa.get('name').title().split(' ')[0]
+  except Exception as e:
+    print("Unable to login",e)
+angel_login()
+st.header(f"Welcome {st.session_state['user_name']}!!!")
