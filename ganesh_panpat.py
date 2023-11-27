7b2ecd996454d7483991212eb08376e505203b1e
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-11-27 18:43:28 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -1,3 +1,9 @@
 import streamlit as st
 import datetime
+import time
 st.header("Welcome Ganesh Panpat")
+current_time=st.empty()
+for i in range(60):
+  current_time.text(datetime.datetime.now().replace(microsecond=0))
+  time.sleep(1)
+
