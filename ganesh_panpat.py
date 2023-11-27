e4187c4df6717c4e7db87cd8d017455415319f25
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-11-27 18:45:16 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import datetime
 import time
+st.set_page_config(page_title="Algo App",layout="wide",initial_sidebar_state="expanded", )
 st.header("Welcome Ganesh Panpat")
 current_time=st.empty()
 for i in range(60):
