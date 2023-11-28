3264e8d4cffb3c2e378ee474eb9d043d444a2781
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-11-29 02:26:51 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -71,4 +71,4 @@ st.write(f"Last Login {st.session_state['login_time']}")
 nf_ce_btn=st.button(label="NIFTY CE")
 if nf_ce_btn:
   odrbook=obj.orderBook()['data']
-  st.table(pd.DataFrame(odrbook))
+  st.write((odrbook))
