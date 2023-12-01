9c5abf6ec9c5b371da83c89cac456165526c9dc4
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-12-01 11:12:57 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -28,6 +28,7 @@ st.markdown("""
   </style>
   """, unsafe_allow_html=True)
 user="Ganesh"
+if 'algo_state' not in st.session_state:st.session_state['algo_state']="Not Running"
 def get_user_pwd(user):
   if user=='Ganesh': username = 'G93179'; pwd = '4789'; apikey = 'CjOKjC5g'; token='U4EAZJ3L44CNJHNUZ56R22TPKI'
   elif user=='Kalyani': username = 'K205244'; pwd = '4789'; apikey = 'lzC7yJmt'; token='YDV6CJI6BEU3GWON7GZTZNU3RM'
@@ -48,9 +49,8 @@ if 'user_name' not in st.session_state:
     st.session_state['feed_token']=obj.feed_token
     st.session_state['userId']=obj.userId
     st.session_state['api_key']=apikey
-    
 st.header(f"Welcome {st.session_state['user_name']}")
-st.write(f"Last Login {st.session_state['login_time']}")
+st.write(f"Last Login {st.session_state['login_time']} Algo : {st.session_state['algo_state']}")
 placeholder = st.empty()
 placeholder.text('LTP')
 obj=SmartConnect(api_key=st.session_state['api_key'],
@@ -130,171 +130,6 @@ def print_ltp():
   except Exception as e:
     return "Unable to get LTP"
 placeholder.text(print_ltp())
-
-@st.cache_resource
-def get_token_df():
-  url = 'https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json'
-  d = requests.get(url).json()
-  token_df = pd.DataFrame.from_dict(d)
-  token_df['expiry'] = pd.to_datetime(token_df['expiry']).apply(lambda x: x.date())
-  token_df = token_df.astype({'strike': float})
-  st.session_state['token_df']=token_df
-  now_dt=datetime.datetime.now(tz=gettz('Asia/Kolkata')).date()-datetime.timedelta(days=0)
-  expiry_df=token_df
-  expiry_df = token_df[(token_df['name'] == 'BANKNIFTY') & (token_df['instrumenttype'] == 'OPTIDX') & (token_df['expiry']>=now_dt)]
-  expiry_day = expiry_df['expiry'].min()
-  bnf_expiry_df = token_df[(token_df['name'] == 'BANKNIFTY') & (token_df['instrumenttype'] == 'OPTIDX') & (token_df['expiry']>=now_dt)]
-  bnf_expiry_day = bnf_expiry_df['expiry'].min()
-  nf_expiry_df = token_df[(token_df['name'] == 'NIFTY') & (token_df['instrumenttype'] == 'OPTIDX') & (token_df['expiry']>=now_dt)]
-  nf_expiry_day = nf_expiry_df['expiry'].min()
-  monthly_expiry_df = token_df[(token_df['name'] == 'BANKNIFTY') & (token_df['instrumenttype'] == 'FUTIDX')  & (token_df['expiry']>=now_dt)]
-  monthly_expiry_day = monthly_expiry_df['expiry'].min()
-
-get_token_df()
-
-def getTokenInfo (symbol, exch_seg ='NSE',instrumenttype='OPTIDX',strike_price = 0,pe_ce = 'CE',expiry_day = None):
-  if symbol=="BANKNIFTY" or symbol=="^NSEBANK":
-    expiry_day=st.session_state['bnf_expiry_day']
-  elif symbol=="NIFTY" or symbol=="^NSEI":
-    expiry_day=st.session_state['nf_expiry_day']
-  df = st.session_state['token_df']
-  strike_price = strike_price*100
-  if exch_seg == 'NSE':
-      eq_df = df[(df['exch_seg'] == 'NSE') ]
-      return eq_df[eq_df['name'] == symbol]
-  elif exch_seg == 'NFO' and ((instrumenttype == 'FUTSTK') or (instrumenttype == 'FUTIDX')):
-      return df[(df['exch_seg'] == 'NFO') & (df['instrumenttype'] == instrumenttype) & (df['name'] == symbol)].sort_values(by=['expiry'])
-  elif exch_seg == 'NFO' and (instrumenttype == 'OPTSTK' or instrumenttype == 'OPTIDX'):
-      return (df[(df['exch_seg'] == 'NFO') & (df['expiry']==expiry_day) &  (df['instrumenttype'] == instrumenttype) & (df['name'] == symbol)
-      & (df['strike'] == strike_price) & (df['symbol'].str.endswith(pe_ce))].sort_values(by=['expiry']))
-
-def get_ce_pe_data(symbol,indexLtp="-"):
-  indexLtp=float(indexLtp) if indexLtp!="-" else get_index_ltp(symbol)
-  # ATM
-  if symbol=='BANKNIFTY' or symbol=='^NSEBANK':
-    symbol='BANKNIFTY'
-    ATMStrike = math.floor(indexLtp/100)*100
-    expiry_day=bnf_expiry_day
-  elif symbol=='NIFTY' or symbol=='^NSEI':
-    symbol='NIFTY'
-    val2 = math.fmod(indexLtp, 50)
-    val3 = 50 if val2 >= 25 else 0
-    ATMStrike = indexLtp - val2 + val3
-    expiry_day=nf_expiry_day
-  #CE,#PE
-  ce_strike_symbol = getTokenInfo(symbol,'NFO','OPTIDX',ATMStrike,'CE',expiry_day).iloc[0]
-  pe_strike_symbol = getTokenInfo(symbol,'NFO','OPTIDX',ATMStrike,'PE',expiry_day).iloc[0]
-  return indexLtp, ce_strike_symbol,pe_strike_symbol
-
-def get_index_ltp(symbol):
-  symbol_i="^NSEI"
-  if symbol=="BANKNIFTY" or symbol=="^NSEBANK" or symbol[:4]=="BANK": symbol="BANKNIFTY"; symbol_i="^NSEBANK"
-  elif symbol=="NIFTY" or symbol=="^NSEI" or symbol[:4]=='NIFT': symbol="NIFTY"; symbol_i="^NSEI"
-  try:
-    data=yf.Ticker(symbol_i).history(interval='1m',period='1d')
-    indexLtp=round(float(data['Close'].iloc[-1]),2)
-    #indexLtp=int(yf.Ticker(symbol_i).fast_info['last_price'])
-  except Exception as e:
-    try:
-      data=yf.Ticker(symbol_i).history(interval='1m',period='1d')
-      indexLtp=int(data['Close'].iloc[-1])
-    except Exception as e:
-       try:
-          spot_token=getTokenInfo_index(symbol).iloc[0]['token']
-          ltpInfo = obj.ltpData('NSE',symbol,spot_token)
-          indexLtp = ltpInfo['data']['ltp']
-       except Exception as e:
-          print('Error get_index_ltp :',e)
-  return indexLtp
-
-def getTokenInfo_index(symbol):
-  df = token_df
-  return df[(df['exch_seg'] == 'NSE') & (df['symbol']== (symbol))]
-
-def get_ltp_price(symbol="-",token="-",exch_seg='-'):
-  try:
-    data=obj.getMarketData("LTP",{exch_seg:[token]})['data']['fetched'][0]['ltp']
-    return data
-  except:
-    try:
-      #if symbol=="-" or token=="-" or exch_seg=="-":
-      df_b=token_df[((token_df['exch_seg']=="NSE") | (token_df['exch_seg']=="NFO"))]
-      df_b=df_b[(df_b['symbol'].str.startswith(symbol))]
-      if len(df_b)!=1: df_b=df_b[((df_b['symbol'].str.startswith(symbol)) | (df_b['name'].str.startswith(symbol)))]
-      if len(df_b)!=1: df_b=df_b[(df_b['name']==symbol) & (token_df['exch_seg']=="NSE")]
-      if len(df_b)!=1: df_b=df_b[df_b['symbol'].str.contains('-EQ')]
-      df_a=token_df[(token_df['token']==str(token)) & ((token_df['exch_seg']=="NSE") | (token_df['exch_seg']=="NFO"))]
-      df_a=df_a.append(df_b)
-      df_a=df_a.drop_duplicates()
-      symbol=df_a['symbol'].iloc[0]
-      token=df_a['token'].iloc[0]
-      name=df_a['name'].iloc[0]
-      exch_seg=df_a['exch_seg'].iloc[0]
-      try:
-        ltpInfo = obj.ltpData(exch_seg,symbol,token)
-        ltp_price = ltpInfo['data']['ltp']
-      except Exception as e:
-          ltp_price=0
-          print("Unable to get LTP")
-    except Exception as e:
-      ltp_price=0
-      print("Unable to get LTP")
-    return ltp_price
-          
-def place_order(token,symbol,qty,buy_sell,ordertype='MARKET',price=0,variety='NORMAL',exch_seg='NFO',producttype='CARRYFORWARD',
-                triggerprice=0,squareoff=0,stoploss=0,ordertag='-'):
-  for i in range(0,3):
-    try:
-      orderparams = {"variety": variety,"tradingsymbol": symbol,"symboltoken": token,"transactiontype": buy_sell,"exchange": exch_seg,
-        "ordertype": ordertype,"producttype": producttype,"duration": "DAY","price": int(float(price)),"squareoff":int(float(squareoff)),
-        "stoploss": int(float(stoploss)),"quantity": str(qty),"triggerprice":int(float(triggerprice)),"ordertag":ordertag,"trailingStopLoss":5}
-      orderId=obj.placeOrder(orderparams)
-      LTP_Price=round(float(get_ltp_price(symbol=symbol,token=token,exch_seg=exch_seg)),2)
-      print(f'{buy_sell} Order Placed: {orderId} Symbol: {symbol} LTP: {LTP_Price} Ordertag: {ordertag}')
-      return orderId,LTP_Price
-      break
-    except Exception as e:
-      print("Order placement failed: ",e)
-      orderId='Order placement failed'
-      LTP_Price='Order placement failed'
-  return orderId,LTP_Price
-                  
-def buy_option(symbol,indicator_strategy,interval,index_sl="-"):
-  try:
-    option_token=symbol['token']
-    option_symbol=symbol['symbol']
-    lotsize=int(symbol['lotsize'])
-    orderId,ltp_price=place_order(token=option_token,symbol=option_symbol,qty=lotsize,buy_sell='BUY',ordertype='MARKET',price=0,
-                          variety='NORMAL',exch_seg='NFO',producttype='CARRYFORWARD',ordertag=indicator_strategy)
-    buy_msg=(f'Buy: {option_symbol}\nLTP: {ltp_price}\n{indicator_strategy}')
-    telegram_bot_sendtext(buy_msg)
-  except Exception as e:pass
-
-def manual_buy(index_symbol,ce_pe="CE",index_ltp="-"):
-  if index_ltp=="-":indexLtp=get_index_ltp(index_symbol)
-  indexLtp, ce_strike_symbol,pe_strike_symbol=get_ce_pe_data(index_symbol,indexLtp=indexLtp)
-  if ce_pe=="CE":symbol=ce_strike_symbol
-  if ce_pe=="PE":symbol=pe_strike_symbol
-  buy_option(symbol,"Manual Buy","5m")
-
-def telegram_bot_sendtext(bot_message):
-  BOT_TOKEN = '5051044776:AAHh6XjxhRT94iXkR4Eofp2PPHY3Omk2KtI'
-  BOT_CHAT_ID = '-1001542241163'
-  BOT_CHAT_ONE_MINUTE = '-1001542241163'
-  #BOT_CHAT_ONE_MINUTE='-882387563'
-  import requests
-  bot_message=st.session_state['user_name']+':\n'+bot_message
-  matches = ["Candle"]
-  if any(x in bot_message for x in matches): check_msg=True
-  else: check_msg=False
-  if check_msg==True:
-    send_text = 'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + BOT_CHAT_ONE_MINUTE + \
-              '&parse_mode=HTML&text=' + bot_message
-  else:
-    send_text = 'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + BOT_CHAT_ID + \
-                '&parse_mode=HTML&text=' + bot_message
-  response = requests.get(send_text)
-        
 if nf_ce:manual_buy("NIFTY",ce_pe="CE",index_ltp="-")
 if nf_pe:manual_buy("NIFTY",ce_pe="PE",index_ltp="-")
 if bnf_ce:manual_buy("BANKNIFTY",ce_pe="CE",index_ltp="-")
