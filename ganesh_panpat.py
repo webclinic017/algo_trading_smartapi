60fb1b52be87d15ff5260aa151856742d54713ec
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-12-01 13:18:52 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -703,6 +703,7 @@ def first_trade_new():
 def manual_buy(index_symbol,ce_pe="CE",index_ltp="-"):
   if index_ltp=="-":indexLtp=get_index_ltp(index_symbol)
   indexLtp, ce_strike_symbol,pe_strike_symbol=get_ce_pe_data(index_symbol,indexLtp=indexLtp)
+  st.write(indexLtp, ce_strike_symbol,pe_strike_symbol)
   if ce_pe=="CE":symbol=ce_strike_symbol
   if ce_pe=="PE":symbol=pe_strike_symbol
   buy_option(symbol,"Manual Buy","5m")
