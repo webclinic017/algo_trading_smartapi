617bb253e481d27745d1ae6edef574bc5b6d3707
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-12-07 12:17:41 +0530

    Update akshay_patil.py

--- akshay_patil.py
+++ akshay_patil.py
@@ -90,8 +90,7 @@ with col2:
       lots_to_trade=st.number_input(label="Lots To Trade",min_value=1, max_value=10, value=1, step=None)
     with col4_tab4:
       target_type = st.selectbox('Target Type',('Points', 'Per Cent'),0)
-
-@st.cache_resource
+      
 def get_token_df():
   url = 'https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json'
   d = requests.get(url).json()
@@ -100,9 +99,9 @@ def get_token_df():
   token_df = token_df.astype({'strike': float})
   st.session_state['token_df']=token_df
   return token_df
-token_df=get_token_df()
-
-@st.cache_resource
+if 'token_df' not in st.session_state:
+  token_df=get_token_df()
+  
 def get_expiry_day_fut_token():
   now_dt=datetime.datetime.now(tz=gettz('Asia/Kolkata')).date()-datetime.timedelta(days=0)
   token_df=st.session_state['token_df']
@@ -117,9 +116,9 @@ def get_expiry_day_fut_token():
   st.session_state['expiry_day'] = expiry_day
   st.session_state['bnf_expiry_day'] = bnf_expiry_day
   return expiry_day,nf_expiry_day,bnf_expiry_day
-
-get_expiry_day_fut_token()
-if 'nf_expiry_day' not in st.session_state:get_expiry_day_fut_token()
+if 'nf_expiry_day' not in st.session_state:
+  get_expiry_day_fut_token()
+  
 def getTokenInfo (symbol, exch_seg ='NSE',instrumenttype='OPTIDX',strike_price = 0,pe_ce = 'CE',expiry_day = None):
   if symbol=="BANKNIFTY" or symbol=="^NSEBANK":expiry_day=st.session_state['bnf_expiry_day']
   elif symbol=="NIFTY" or symbol=="^NSEI":expiry_day=st.session_state['nf_expiry_day']
