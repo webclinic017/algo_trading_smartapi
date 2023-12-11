314afc549bb1e4263909bd21d7c6d2d87bd404ef
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-12-11 19:44:27 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -182,7 +182,7 @@ def update_order_book():
     else:
       orderbook=pd.DataFrame(orderbook)
       orderbook=orderbook.sort_values(by = ['updatetime'], ascending = [False], na_position = 'first')
-      orderbook=update_price_orderbook(orderbook)
+      #orderbook=update_price_orderbook(orderbook)
       orderbook['price']=round(orderbook['price'].astype(int),2)
       order_datatable.table(orderbook[['updatetime','orderid','transactiontype','status','tradingsymbol','price','quantity','ordertag']])
       return orderbook
