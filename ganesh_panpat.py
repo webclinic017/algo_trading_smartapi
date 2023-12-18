88d3c876bc804e217aa5c36a1a8decef8b4e6a40
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-12-19 01:04:34 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -716,36 +716,38 @@ def get_near_option_list():
         st.write(f'{i}')
 def near_option_trade(interval):
   options_trade=[]
-  try:
-    for i in range(0,3):
-      for symbol in ['BANKNIFTY','NIFTY']:
-        for ce_pe in ['CE','PE']:
-          if symbol=='BANKNIFTY':
-            expiry_day=st.session_state['bnf_expiry_day']
-            index_ltp=st.session_state['BankNifty']
-            diff=100
-          elif symbol=='NIFTY':
-            expiry_day=st.session_state['nf_expiry_day']
-            index_ltp=st.session_state['Nifty']
-            diff=50
-          index_ltp=diff*round(index_ltp/diff)
-          if ce_pe=="CE":strike_price=index_ltp+(i*diff)
-          else:strike_price=index_ltp-(i*diff)
-          opt_symbol=symbol+expiry_day+str(int(strike_price))+ce_pe
-          strike_symbol=obj.searchScrip("NFO",opt_symbol)['data'][0]
-          token=strike_symbol['symboltoken']
-          symbol=strike_symbol['tradingsymbol']
-          fut_data=get_historical_data(symbol,interval=interval,token=token,exch_seg='NFO')
-          indicator_strategy=fut_data['Indicator'].values[-1]
-          if (fut_data['St Trade'].values[-1]=="Buy" or fut_data['ST_10_2 Trade'].values[-1]=="Buy"):
-            buy_option(strike_symbol,indicator_strategy,"5m")
-          information={'Time':str(datetime.datetime.now(tz=gettz('Asia/Kolkata')).time().replace(microsecond=0)),'Symbol':opt_symbol,
-                   'Datetime':str(fut_data['Datetime'].values[-1]),'Close':fut_data['Close'].values[-1],'Indicator':fut_data['Indicator'].values[-1],
-                   'Trade':fut_data['Trade'].values[-1]}
-          options_trade.append(information)
-  except Exception as e:
-    logger.exception(f"Error in near_option_trade: {e}")
-    pass
+  for i in range(0,3):
+    for symbol in ['BANKNIFTY','NIFTY']:
+      for ce_pe in ['CE','PE']:
+        if symbol=='BANKNIFTY':
+          expiry_day=st.session_state['bnf_expiry_day']
+          index_ltp=st.session_state['BankNifty']
+          diff=100
+        elif symbol=='NIFTY':
+          expiry_day=st.session_state['nf_expiry_day']
+          index_ltp=st.session_state['Nifty']
+          diff=50
+        index_ltp=diff*round(index_ltp/diff)
+        for j in range(0,2):
+          try:
+            if ce_pe=="CE":strike_price=index_ltp+(i*diff)
+            else:strike_price=index_ltp-(i*diff)
+            opt_symbol=symbol+expiry_day+str(int(strike_price))+ce_pe
+            strike_symbol=obj.searchScrip("NFO",opt_symbol)['data'][0]
+            token=strike_symbol['symboltoken']
+            symbol=strike_symbol['tradingsymbol']
+            fut_data=get_historical_data(symbol,interval=interval,token=token,exch_seg='NFO')
+            indicator_strategy=fut_data['Indicator'].values[-1]
+            if (fut_data['St Trade'].values[-1]=="Buy" or fut_data['ST_10_2 Trade'].values[-1]=="Buy"):
+              buy_option(strike_symbol,indicator_strategy,"5m")
+            information={'Time':str(datetime.datetime.now(tz=gettz('Asia/Kolkata')).time().replace(microsecond=0)),'Symbol':opt_symbol,
+                         'Datetime':str(fut_data['Datetime'].values[-1]),'Close':fut_data['Close'].values[-1],'Indicator':fut_data['Indicator'].values[-1],
+                         'Trade':fut_data['Trade'].values[-1]}
+            options_trade.append(information)
+            break
+          except Exception as e:
+            logger.exception(f"Error in near_option_trade: {e}")
+            pass
   st.session_state['options_trade_list']=options_trade
 
 def update_todays_trade(todays_trade_log):
