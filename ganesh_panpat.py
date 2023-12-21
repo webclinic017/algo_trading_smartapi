a3b2ae86b92651488d71c06a19a275dc05657388
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2023-12-21 17:56:19 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -221,7 +221,7 @@ def update_order_book():
       #orderbook=update_ltp_buy_df(orderbook)
       order_book_updated.text(f"Orderbook : {datetime.datetime.now(tz=gettz('Asia/Kolkata')).replace(microsecond=0, tzinfo=None).time()}")
       #order_datatable.table(orderbook[['updatetime','orderid','transactiontype','status','tradingsymbol','price','quantity','ordertag']])
-      order_datatable.dataframe(orderbook[['updatetime','orderid','transactiontype','status','tradingsymbol','price','quantity','ordertag']])
+      order_datatable.dataframe(orderbook[['updatetime','orderid','transactiontype','status','tradingsymbol','price','quantity','ordertag']],hide_index=True)
       orderbook=orderbook[['updatetime','orderid','transactiontype','status','symboltoken','tradingsymbol','price','quantity','ordertag']]
     return orderbook
   except Exception as e:
