2fc0461bbbd7055f44432fdfb6a8b9708e91074e
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-01-17 23:55:55 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -437,7 +437,7 @@ def get_historical_data(symbol="-",interval='5m',token="-",exch_seg="-",candle_t
     now=datetime.datetime.now(tz=gettz('Asia/Kolkata')).replace(microsecond=0, tzinfo=None)
     last_candle=now.replace(second=0, microsecond=0)- datetime.timedelta(minutes=delta_time)
     algo_datatable=st.dataframe(df)
-    df = df[(df.index <= last_candle)]
+    #df = df[(df.index <= last_candle)]
     df['Time Frame']=odd_interval
     df.index.names = ['']
     df['VWAP']=pdta.vwap(high=df['High'],low=df['Low'],close=df['Close'],volume=df['Volume'])
