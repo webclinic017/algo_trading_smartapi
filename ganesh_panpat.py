3e26a3ad14df522961e02752d453ba1763a71fa0
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-01-19 19:08:21 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -1004,5 +1004,5 @@ update_position()
 if nf_ce:
   multiline_content_list = ["Line 1", "Line 2", "Line 3", "Line 4"]
   multiline_text = "\n".join(multiline_content_list)
-  algo_log.text(multiline_text)
+  algo_log.text=multiline_text
   #st.text_area("Multiline Text", multiline_text)
