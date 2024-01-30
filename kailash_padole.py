6b8ea6f442e9ce89c4dfe931fb5755c49e89dad3
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-01-30 23:37:45 +0530

    Update kailash_padole.py

--- kailash_padole.py
+++ kailash_padole.py
@@ -920,7 +920,7 @@ with tab0:
         bnf_pe=st.button(label="BNF PE")
         close_all=st.button("Close All")
         restart=st.button("Restart")
-        algo_state=st.checkbox("Run Algo")
+        algo_state=st.checkbox("Run Algo",value=True)
     with col2:
         log_holder=st.empty()
 with tab1:
