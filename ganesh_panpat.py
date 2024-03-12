4797d8ffb3a7d6d1f656f9c33899c0c973966348
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-03-12 14:40:20 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -825,11 +825,9 @@ def is_within_20_minute_gap(target_time):
 def future_trade():
   token_df=st.session_state['fut_list']
   for symbol in fut_list:
-    print(symbol)
     fut_token_details=token_df[(token_df['name'] == symbol)].sort_values(by=['expiry'], ascending=True)
     for i in range(0,len(fut_token_details)):
       token_details=fut_token_details.iloc[i]
-      print(token_details)
       fut_data=get_historical_data(symbol=token_details['symbol'],interval='5m',token=token_details['token'],exch_seg=token_details['exch_seg'],candle_type="NORMAL")
       trade=str(fut_data['Trade'].values[-1])
       if trade!="-" and is_within_20_minute_gap(fut_data.iloc[-1].name):
