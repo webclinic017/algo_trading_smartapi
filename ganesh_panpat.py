78eba9dc57ba6e6656ed8ae9dc83cfeef8dcd700
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-03-12 14:34:04 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -1084,6 +1084,7 @@ if bnf_pe:
   indexLtp, ce_strike_symbol,pe_strike_symbol=get_ce_pe_data('BANKNIFTY',indexLtp='-')
   buy_option(pe_strike_symbol,'Manual Buy','5m')
 if close_all:
+  future_trade()
   closing_trade()
 
 position,open_position=get_open_position()
