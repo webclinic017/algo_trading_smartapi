cd0a1bb9061579c89f3340210b09a5bf85fc5b2c
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-04-11 13:43:06 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -5,7 +5,45 @@ st.markdown("""
     .block-container {padding-top: 0.5rem;padding-bottom: 0rem;padding-left: 2rem;padding-right: 5rem;}
   </style>
   """, unsafe_allow_html=True)
-#Define App
-st.session_state['user_name']="New User"
-st.header(f"Welcome {st.session_state['user_name']}")
-st.header(f"Hi {st.session_state['user_name']}")
+
+from SmartApi import SmartConnect
+from SmartApi import SmartWebSocket
+from SmartApi.smartWebSocketV2 import SmartWebSocketV2
+import threading; import pandas as pd
+import pandas_ta as pdta
+import json
+import requests
+import datetime
+from dateutil.tz import gettz
+import time
+import math
+import numpy
+import warnings
+import yfinance as yf
+import sys
+import pyotp
+from logzero import logger
+from py5paisa import FivePaisaClient
+import re
+import requests
+pd.options.mode.chained_assignment = None
+warnings.filterwarnings('ignore')
+NoneType = type(None)
+
+username=st.secrets["username"]
+pwd=st.secrets["pwd"]
+apikey=st.secrets["apikey"]
+token=st.secrets["token"]
+user=st.secrets["user"]
+
+#Telegram Msg
+def telegram_bot_sendtext(bot_message):
+  BOT_TOKEN = '5051044776:AAHh6XjxhRT94iXkR4Eofp2PPHY3Omk2KtI'
+  BOT_CHAT_ID = '-1001542241163'
+  bot_message=st.secrets["user"]+':\n'+bot_message
+  send_text = 'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + BOT_CHAT_ID + '&parse_mode=HTML&text=' + bot_message
+  response = requests.get(send_text)
+
+telegram_bot_sendtext("HI")
+
+
