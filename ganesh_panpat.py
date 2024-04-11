259bab6242341ce31c13e61dd297fbef38021896
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-04-11 15:51:29 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -975,4 +975,66 @@ st.header(f"Welcome {st.session_state['user_name']}")
 last_login=st.empty()
 last_login.text(f"Login: {st.session_state['login_time']}")
 index_ltp_string=st.empty()
-index_ltp_string.text(f"Index Ltp: \{print_ltp()}")
+index_ltp_string.text(f"Index Ltp: {print_ltp()}")
+tab0, tab1, tab2, todays_trade,tab3, tab4,tab5,tab6,back_test= st.tabs(["Log","Order Book", "Position","Todays Trade","Open Order", "Settings","Token List","Future List","Back Test"])
+with tab0:
+  col1,col2=st.columns([1,9])
+  with col1:
+    nf_ce=st.button(label="NF CE")
+    bnf_ce=st.button(label="BNF CE")
+    nf_pe=st.button(label="NF PE")
+    bnf_pe=st.button(label="BNF PE")
+    close_all=st.button("Close All")
+    restart=st.button("Restart")
+    algo_state=st.checkbox("Run Algo")
+  with col2:
+    log_holder=st.empty()
+with tab1:
+  order_book_updated=st.empty()
+  order_book_updated.text(f"Orderbook : ")
+  order_datatable=st.empty()
+with tab2:
+  position_updated=st.empty()
+  position_updated.text(f"Position : ")
+  position_datatable=st.empty()
+with tab3:
+  open_order_updated=st.empty()
+  open_order_updated.text(f"Open Order : ")
+  open_order=st.empty()
+with tab4:
+  ind_col1,ind_col2,ind_col3,ind_col4=st.columns([5,1.5,1.5,1.5])
+  indicator_list=['St Trade', 'ST_10_2 Trade','ST_10_1 Trade', 'RSI MA Trade','RSI_60 Trade','MACD Trade','PSAR Trade','DI Trade',
+                  'MA Trade','EMA Trade','EMA_5_7 Trade','MA 21 Trade','HMA Trade','RSI_60 Trade','EMA_High_Low Trade','Two Candle Theory','TEMA_EMA_9 Trade']
+  with ind_col1:
+    index_list=st.multiselect('Select Index',['NIFTY','BANKNIFTY','SENSEX'],['NIFTY','BANKNIFTY','SENSEX'])
+    fut_list=st.multiselect('Select Future',['SILVERMIC','SILVER'],[])
+    time_frame_interval = st.multiselect('Select Time Frame',['IDX:5M', 'IDX:15M', 'OPT:5M', 'OPT:15M','IDX:1M'],['IDX:5M','OPT:5M'])
+    five_buy_indicator = st.multiselect('Index Indicator',indicator_list,['St Trade', 'ST_10_2 Trade','MACD Trade','TEMA_EMA_9 Trade'])
+    option_buy_indicator = st.multiselect('Option Indicator',indicator_list,['St Trade', 'ST_10_2 Trade'])
+    #three_buy_indicator = st.multiselect('Three Minute Indicator',indicator_list,[])
+    #one_buy_indicator = st.multiselect('One Minute Indicator',indicator_list,[])
+  with ind_col2:
+    target_order_type = st.selectbox('Target Order',('Target', 'Stop_Loss', 'NA'),1)
+    target_type = st.selectbox('Target Type',('Points', 'Per Cent','Indicator'),1)
+    if target_type!="Indicator":
+        sl_point=st.number_input(label="SL",min_value=10, max_value=100, value=30, step=None)
+        target_point=st.number_input(label="Target",min_value=5, max_value=100, value=50, step=None)
+  with ind_col3:
+    lots_to_trade=st.number_input(label="Lots To Trade",min_value=1, max_value=10, value=1, step=None)
+  with ind_col4:
+    st.date_input("BNF Exp",st.session_state['bnf_expiry_day'])
+    st.date_input("NF Exp",st.session_state['nf_expiry_day'])
+    st.date_input("FIN NF Exp",st.session_state['fnnf_expiry_day'])
+    st.date_input("SENSEX Exp",st.session_state['sensex_expiry_day'])
+  with tab5:
+  token_df=st.empty()
+  token_df=st.dataframe(st.session_state['opt_list'],hide_index=True)
+with tab6:
+  fut_token_df=st.empty()
+  fut_token_df=st.dataframe(st.session_state['fut_list'],hide_index=True)
+with back_test:
+  hourly_scan=st.button("Hourly ST Scan")
+  daily_backtest=st.button("Todays Trade")
+  download_data=st.button("Download Historical Data")
+if algo_state:
+  loop_code()
