dbd5b51fc2dd1cfea3329a5ba3b5a7dea791192d
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-04-18 11:45:26 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -1159,13 +1159,14 @@ def loop_code():
       now=datetime.datetime.now(tz=gettz('Asia/Kolkata'))
       while now.second <50:
         index_ltp_string.text(f"Index Ltp: {print_ltp()}")
-        todays_trade=update_ltp_buy_df(todays_trade)
-        todays_trade=check_pnl_todays_trade(todays_trade)
-        pnl=int(todays_trade['Profit'].sum())
-        n_buy_df=todays_trade[['updatetime','tradingsymbol','price','quantity','ordertag','Exit Time','Status', 'Sell', 'ltp', 'Profit','Target',
+        if now.second <30
+          todays_trade=update_ltp_buy_df(todays_trade)
+          todays_trade=check_pnl_todays_trade(todays_trade)
+          pnl=int(todays_trade['Profit'].sum())
+          n_buy_df=todays_trade[['updatetime','tradingsymbol','price','quantity','ordertag','Exit Time','Status', 'Sell', 'ltp', 'Profit','Target',
                                'Stop Loss', 'Profit %', 'Sell Indicator']]
-        todays_trade_datatable.dataframe(n_buy_df,hide_index=True)
-        todays_trade_updated.text(f"Todays Trade : {datetime.datetime.now(tz=gettz('Asia/Kolkata')).time().replace(microsecond=0)} PNL: {pnl}")
+          todays_trade_datatable.dataframe(n_buy_df,hide_index=True)
+          todays_trade_updated.text(f"Todays Trade : {datetime.datetime.now(tz=gettz('Asia/Kolkata')).time().replace(microsecond=0)} PNL: {pnl}")
         now=datetime.datetime.now(tz=gettz('Asia/Kolkata'))
         time.sleep(1)
       now=datetime.datetime.now(tz=gettz('Asia/Kolkata'))
