ab40512567bc60783b59736455699655220b783d
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-04-29 10:38:47 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -8,37 +8,13 @@ st.markdown("""
 from SmartApi import SmartConnect
 import pyotp
 from logzero import logger
-apikey = 'Rz6IiOsd'
-username = 'G93179'
-pwd = '4789'
-token='U4EAZJ3L44CNJHNUZ56R22TPKI'
-
-for attempt in range(3):
-  try:
-    print(f"Login attempt {attempt}")
-    obj = SmartConnect(apikey)
-    try:totp = pyotp.TOTP(token).now()
-    except Exception as e:
-      time.sleep(2)
-      logger.error("Invalid Token: The provided token is not valid.")
-      raise e
-      correlation_id = "abcde"
-    data = obj.generateSession(username, pwd, totp)
-    if data['status'] == False:logger.error(data)
-    else:
-      authToken = data['data']['jwtToken']
-      refreshToken = data['data']['refreshToken']
-      feedToken = obj.getfeedToken()
-      res = obj.getProfile(refreshToken)
-      obj.generateToken(refreshToken)
-      userProfile= obj.getProfile(refreshToken)
-      aa= userProfile.get('data')
-      print(f"Welcome {aa.get('name').title()}")
-      userProfile= obj.getProfile(refreshToken)
-      aa= userProfile.get('data')
-      print(f"Welcome {aa.get('name').title()}...")
-      st.write(aa.get('name').title())
-      break
-  except Exception as e:
-    time.sleep(2)
-    print(f"Unable to login error in angel_login {e}")
+from py5paisa import FivePaisaClient
+cred={
+    "APP_NAME":"5PGANESH",
+    "APP_SOURCE":"151",
+    "USER_ID":"jGpPvkKRRBu",
+    "PASSWORD":"CH552StL6Np",
+    "USER_KEY":"EWZRvRh5TTs6uaJcfngDwx1u3VMZ3EaQ",
+    "ENCRYPTION_KEY":"gdH8fPb3H3oOAh8nYAA8GdmNtCpsMHVFdPfcyaxNiWiKoz1WSkbRSqoffmF6yIje"}
+client = FivePaisaClient(cred=cred)
+client.get_totp_session('53079505',pyotp.TOTP('GUZTANZZGUYDKXZVKBDUWRKZ').now(),'478963')
