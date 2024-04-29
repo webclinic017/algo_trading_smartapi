ed82b7f08923529c7fba8c30eeeaf59fbb847748
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-04-29 10:36:37 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -8,32 +8,37 @@ st.markdown("""
 from SmartApi import SmartConnect
 import pyotp
 from logzero import logger
-api_key = 'Rz6IiOsd'
+apikey = 'Rz6IiOsd'
 username = 'G93179'
 pwd = '4789'
-smartApi = SmartConnect(api_key)
-try:
-    token = "U4EAZJ3L44CNJHNUZ56R22TPKI"
-    totp = pyotp.TOTP(token).now()
-except Exception as e:
-    logger.error("Invalid Token: The provided token is not valid.")
-    raise e
-correlation_id = "abcde"
-data = smartApi.generateSession(username, pwd, totp)
-if data['status'] == False:logger.error(data)
-else:
-  # login api call
-  # logger.info(f"You Credentials: {data}")
-  authToken = data['data']['jwtToken']
-  refreshToken = data['data']['refreshToken']
-  # fetch the feedtoken
-  feedToken = smartApi.getfeedToken()
-  # fetch User Profile
-  res = smartApi.getProfile(refreshToken)
-  smartApi.generateToken(refreshToken)
-  res=res['data']['exchanges']
-  userProfile= obj.getProfile(refreshToken)
-  aa= userProfile.get('data')
-  print(f"Welcome {aa.get('name').title()}...")
-st.write(aa.get('name').title())
-  
+token='U4EAZJ3L44CNJHNUZ56R22TPKI'
+
+for attempt in range(3):
+  try:
+    print(f"Login attempt {attempt}")
+    obj = SmartConnect(apikey)
+    try:totp = pyotp.TOTP(token).now()
+    except Exception as e:
+      time.sleep(2)
+      logger.error("Invalid Token: The provided token is not valid.")
+      raise e
+      correlation_id = "abcde"
+    data = obj.generateSession(username, pwd, totp)
+    if data['status'] == False:logger.error(data)
+    else:
+      authToken = data['data']['jwtToken']
+      refreshToken = data['data']['refreshToken']
+      feedToken = obj.getfeedToken()
+      res = obj.getProfile(refreshToken)
+      obj.generateToken(refreshToken)
+      userProfile= obj.getProfile(refreshToken)
+      aa= userProfile.get('data')
+      print(f"Welcome {aa.get('name').title()}")
+      userProfile= obj.getProfile(refreshToken)
+      aa= userProfile.get('data')
+      print(f"Welcome {aa.get('name').title()}...")
+      st.write(aa.get('name').title())
+      break
+  except Exception as e:
+    time.sleep(2)
+    print(f"Unable to login error in angel_login {e}")
