e5857209f853b8692edf23184d637eabe11aa2a8
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-05-02 21:05:54 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -7,6 +7,7 @@ from dateutil.tz import gettz
 import pandas as pd
 import pandas_ta as pdta
 from SmartApi import SmartConnect
+NoneType = type(None)
 LOGGER = get_logger(__name__)
 st.set_page_config(page_title="Algo App",layout="wide",initial_sidebar_state="expanded",)
 st.markdown("""
