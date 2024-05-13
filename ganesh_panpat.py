418eef81368ccfe94d169246a2d8c211cfa43c63
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-05-13 15:23:20 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -961,8 +961,8 @@ def check_pnl_todays_trade(buy_df):
               if now.minute%5==0:
                 dd=get_historical_data(symbol=tradingsymbol,interval='5m',token=symboltoken,exch_seg=exch_seg,candle_type="NORMAL").tail(1)
                 if dd['ST_7_3 Trade'].iloc[0]=="Sell" or dd['Supertrend_10_2'].iloc[0]=="Sell":
-                  exit_position(symboltoken,tradingsymbol,exch_seg,qty,ltp_price,sl,ordertag='Indicaor Exit:'+ordertag,producttype='CARRYFORWARD')
-                  multiline_string = "Indicaor Exit:"+trade_info
+                  exit_position(symboltoken,tradingsymbol,exch_seg,qty,ltp_price,sl,ordertag='Opt Indicaor Exit:'+ordertag,producttype='CARRYFORWARD')
+                  multiline_string = "Opt Indicaor Exit:"+trade_info
                   telegram_bot_sendtext(multiline_string)
                   buy_df['Status'].iloc[i]="Indicaor Exit"
       except Exception as e:print(f"error in check_pnl_todays_trade {e}")
