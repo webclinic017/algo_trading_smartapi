f52ca8ca8d5afa5e47b34eae871d93e946d8b7e3
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-07-01 17:09:27 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -11,7 +11,7 @@ import pandas_ta as pdta
 import warnings
 import yfinance as yf
 import time
-#warnings.filterwarnings('ignore')
+warnings.filterwarnings('ignore')
 NoneType = type(None)
 import math
 st.set_page_config(page_title="Algo App",layout="wide",initial_sidebar_state="expanded",)
