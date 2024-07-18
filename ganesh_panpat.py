ab1c0f76249815747f265b0278f3cc8d61e3000d
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-07-18 15:14:13 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -19,7 +19,7 @@ import math
 st.set_page_config(page_title="Algo App",layout="wide",initial_sidebar_state="expanded",)
 st.markdown("""
   <style>
-    .block-container {padding-top: 0.9rem;padding-bottom: 0rem;padding-left: 2rem;padding-right: 5rem;}
+    .block-container {padding-top: 0.1rem;padding-bottom: 0rem;padding-left: 2rem;padding-right: 5rem;}
   </style>
   """, unsafe_allow_html=True)
 st.write("Welcome To Algo Trading")
