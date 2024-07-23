428490c3d62379f513ba671f0af44a9a4609a4cf
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-07-23 10:57:13 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -809,7 +809,7 @@ def index_trade(symbol,interval):
     if fut_data is None: return None
     trade=str(fut_data['Trade'].values[-1])
     if trade!="-":
-      indicator_strategy=f"{fut_data['Indicator'].values[-1]} Signal: {fut_data['Datetime'].values[-1]}"
+      indicator_strategy=f"{fut_data['Indicator'].values[-1]} [{fut_data['Datetime'].values[-1]}]"
       indexLtp=fut_data['Close'].values[-1]
       indexLtp, ce_strike_symbol,pe_strike_symbol=get_ce_pe_data(symbol,indexLtp=indexLtp)
       if trade=="Buy" : buy_option(ce_strike_symbol,indicator_strategy,interval)
@@ -882,7 +882,7 @@ def trade_near_options(time_frame):
               st.session_state['options_trade_list'].append(information)
               st.session_state['index_trade_end'][symbol_name+"_"+time_frame] = opt_data['Trade'].values[-1]
               if opt_data['Trade'].values[-1]=="Buy":
-                indicator=f"{opt_data['Indicator'].values[-1]} Signal: {opt_data['Datetime'].values[-1]}"
+                indicator=f"{opt_data['Indicator'].values[-1]} [{opt_data['Datetime'].values[-1]}]"
                 strike_symbol=option_list.iloc[i]
                 buy_option(symbol=strike_symbol,indicator_strategy=indicator,interval=time_frame,index_sl="-")
                 break
